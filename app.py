ba59371a78dbb39571bf714595886c993c293591
Theeb12 <ethan.aka.eb@gmail.com>
2024-01-31 13:20:37 -0500

    scrape news
    created a news scraping method that gets headlines from a URL. Updated new.html to display said headlines

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_news():
+    url = "https://en.wikipedia.org/wiki/Fukushima_nuclear_accident#References"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    headlines = []
+    for headline in soup.find_all("h3", class_="headline"):
+        headlines.append(headline.text)
+    return headlines
 
 app = Flask(__name__)
 
@@ -16,8 +27,10 @@ def map():
 
 @app.route('/news')
 def news():
-    return render_template('news.html')
+    headlines = scrape_news()
+    return render_template('news.html', headlines = headlines)
 
 @app.route('/forum')
 def forum():
     return render_template('forum.html')
+
