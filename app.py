6aea2487827dc3adff291f41215f9a78df13ec6d
Eric Xu <ehx207@nyu.edu>
2024-01-31 13:45:25 -0500

    Topics creation setup.

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
 
 app = Flask(__name__)
+
+# SQLAlchemy and DB setup
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db.init_app(app)
+
+class Topic(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    description: Mapped[str]
+
+class Comment():
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str]
+    topicId: Mapped[str]
+
+
+with app.app_context():
+    db.create_all()
 
 # PAGE ROUTES
 
@@ -22,17 +48,54 @@ def map():
 def news():
     return render_template('news.html')
 
-@app.route('/forum')
+@app.route('/forum', methods=["GET", "POST"])
 def forum():
+    if request.method == "POST":
+        topic = Topic(
+            title=request.form["title"],
+            desc=request.form["description"],
+        )
+        db.session.add(topic)
+        db.session.commit()
+        
     return render_template('forum.html')
 
-# SQLAlchemy and DB setup
-
-class Base(DeclarativeBase):
+@app.route('/forum/topic/<int:id>', methods=["GET", "POST"])
+def topic():
     pass
 
-db = SQLAlchemy(model_class=Base)
+"""
+@app.route("/users")
+def user_list():
+    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    return render_template("user/list.html", users=users)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+@app.route("/users/create", methods=["GET", "POST"])
+def user_create():
+    if request.method == "POST":
+        user = User(
+            username=request.form["username"],
+            email=request.form["email"],
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("user_detail", id=user.id))
 
-db.init_app(app)
+    return render_template("user/create.html")
+
+@app.route("/user/<int:id>")
+def user_detail(id):
+    user = db.get_or_404(User, id)
+    return render_template("user/detail.html", user=user)
+
+@app.route("/user/<int:id>/delete", methods=["GET", "POST"])
+def user_delete(id):
+    user = db.get_or_404(User, id)
+
+    if request.method == "POST":
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for("user_list"))
+
+    return render_template("user/delete.html", user=user)
+"""
